ccec8a6e4f46850dd3748cd93982fd9059121452
com-user01 <tsuyoshi-misawa@comcept.jp>
2015-03-09 17:36:54 +0900

    A* ライブラリ＆サンプル追加

---
+++ Assets/AstarPathfindingProject/ExampleScenes/ExampleScripts/Bridge.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour {
+	private const float TO_DESTROY_TIME = 20f;
+	private bool mToDestroy;
+	private bool mFinished;
+	private float mToDestroyTimer;
+	private Rigidbody mRidgit;
+	private Color mSttCol;
+	private readonly Color mEndCol = Color.red;
+	private readonly Color mFinishedCol = Color.black;
+	private Pathfinding.GraphUpdateScene mGUScene;
+	private Vector3[] mPoints;
+
+	// Use this for initialization
+	void Start () {
+		mToDestroy = false;
+		mFinished = false;
+		mToDestroyTimer = TO_DESTROY_TIME;
+		mRidgit = gameObject.GetComponent<Rigidbody> ();
+		mGUScene = gameObject.GetComponent<Pathfinding.GraphUpdateScene> ();
+		mSttCol = GetComponent<Renderer> ().material.color;
+		int divNum = 10;
+		mPoints = new Vector3[divNum];
+		for(int ii = 0; ii < mPoints.Length; ++ii) {
+			Vector3 ppos = new Vector3();
+			ppos.z -= 0.5f; //* transform.localScale.z;
+			ppos.z += ((float)ii/(float)mPoints.Length); // * transform.localScale.z;
+			mPoints[ii] = ppos;
+		}
+//		mGUScene.points = mPoints;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mFinished)
+			return;
+
+		if (!mToDestroy) {
+			if (mRidgit.IsSleeping()) {
+				mToDestroy = true;
+				mGUScene.Apply();
+			}
+		} else {
+			if(!mRidgit.IsSleeping()){
+				mToDestroy = false;
+			}
+			mToDestroyTimer -= Time.deltaTime;
+			float rate = 1f-mToDestroyTimer/TO_DESTROY_TIME;
+			GetComponent<Renderer> ().material.color = Color.Lerp(mSttCol,mEndCol,rate);
+			if(mToDestroyTimer<=0f){
+				mFinished = true;
+				Debug.Log("finished");
+				mGUScene.enabled = false;
+				GetComponent<Renderer> ().material.color = mFinishedCol;
+//				Destroy(gameObject);
+			}
+		}
+	}
+
+}
---
+++ Assets/AstarPathfindingProject/ExampleScenes/ExampleScripts/BridgeGenerator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeGenerator : MonoBehaviour {
+	public GameObject bridgePrefab;
+	public GameObject addBlockPrefab;
+	private Camera mCam;
+	private bool mDrag;
+	private Vector3 mSttPos;
+	private Vector3 mEndPos;
+	private Collider mSttColl;
+	private Collider mEndColl;
+	// Use this for initialization
+	void Start () {
+		mCam = Camera.main;
+		mDrag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray;
+		RaycastHit hit;
+		if (Input.GetMouseButtonDown (0)) {
+			if (!mDrag) {
+				ray = mCam.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (ray, out hit)) {
+					mDrag = true;
+					mSttColl = hit.collider;
+					mSttPos = hit.point;
+				}
+			}
+		} else if (Input.GetMouseButtonUp (0)) {
+			if (mDrag) {
+				mDrag = false;
+				ray = mCam.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (ray, out hit)) {
+					mEndColl = hit.collider;
+					mEndPos = hit.point;
+					if(mSttColl != mEndColl){
+						Vector3 dir = mEndPos-mSttPos;
+						Vector3 pos = mSttPos + dir*0.5f + Vector3.up*5f;
+						Vector3 scale = bridgePrefab.transform.localScale;
+						scale.z = dir.magnitude;
+						GameObject go = Instantiate(bridgePrefab);
+						go.transform.position = pos;
+						go.transform.rotation = Quaternion.LookRotation(dir);
+						go.transform.localScale = scale;
+					}else{
+						GameObject go = Instantiate(addBlockPrefab);
+						go.transform.position = mSttPos + Vector3.up*5f;
+					}
+				}
+			}
+		}
+	
+	}
+}
